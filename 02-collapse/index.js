d6dffaa18ba69e76eae530a88c83c437f92bfed6
Maryna Darashuk <59647513+maridoroshuk@users.noreply.github.com>
2022-08-09 20:32:41 +0300

    feat: added collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,27 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector(".collapsible__button");
+const content = document.querySelector(".collapsible__content");
+const visibleSpan = document.querySelector(".collapsible__action--visible");
+const hidenSpan = document.querySelector(".collapsible__action--hidden");
+
+button.addEventListener("click", hideContent);
+function hideContent(e) {
+	if (content.style.display !== "none") {
+		content.style.display = "none";
+		changeButtonText();
+	} else {
+		content.style.display = "block";
+		changeButtonText();
+	}
+}
+
+function changeButtonText() {
+	if (content.style.display !== "none") {
+		hidenSpan.style.display = "none";
+		visibleSpan.style.display = "block";
+	} else {
+		hidenSpan.style.display = "block";
+		visibleSpan.style.display = "none";
+	}
+}
+
+changeButtonText();
